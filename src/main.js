eff9b28e02653aa00491d3147b269c3a301bb050
zalec <142818531+z4le3c@users.noreply.github>
2023-12-22 18:32:46 -0600

    add gameloop

--- src/main.js
+++ src/main.js
@@ -6,10 +6,10 @@ import { UI } from './ui'
 const genericBoard = () => {
   const board = Gameboard.new()
   board.placeShip(1, 2, Gameboard.VERTICAL, Ship.new(2))
-  board.placeShip(2, 4, Gameboard.HORIZONTAL, Ship.new(5))
-  board.placeShip(4, 3, Gameboard.HORIZONTAL, Ship.new(4))
-  board.placeShip(6, 3, Gameboard.VERTICAL, Ship.new(3))
-  board.placeShip(6, 8, Gameboard.VERTICAL, Ship.new(2))
+  // board.placeShip(2, 4, Gameboard.HORIZONTAL, Ship.new(5))
+  // board.placeShip(4, 3, Gameboard.HORIZONTAL, Ship.new(4))
+  // board.placeShip(6, 3, Gameboard.VERTICAL, Ship.new(3))
+  // board.placeShip(6, 8, Gameboard.VERTICAL, Ship.new(2))
 
   return board
 }
@@ -17,5 +17,35 @@ const genericBoard = () => {
 const player1 = Player.new(genericBoard())
 const computer = Player.new(genericBoard())
 
-UI.renderBoard(player1.board, "b-player")
-UI.renderBoard(computer.board, "b-computer", true)
+const gameManager = {}
+gameManager.playerTurn = true
+gameManager.gameOn = true
+gameManager.endGame = () => {
+  console.log(
+    `game over: ${gameManager.playerTurn ? 'the player won' : 'the computer won'}`
+  )
+  const message = document.querySelector('.message')
+  message.textContent = gameManager.playerTurn ? 'the player won' : 'the computer won'
+}
+gameManager.computerPlay = () => {
+  setTimeout(function(){
+    const [row, col] = Player.randomValidCell(computer)
+    console.log(row, col)
+    const gridCell = document.getElementById(`${row}-${col}`)
+    let hit = computer.attack(row, col, player1)
+    if (hit) {
+      gridCell.style.backgroundColor = 'red'
+      if (player1.board.allSunk()) {
+        gameManager.gameOn = false
+        gameManager.endGame()
+        return
+      }
+      gameManager.computerPlay()
+    } else {
+      gridCell.style.backgroundColor = 'rgb(202, 202, 202)'
+      gameManager.playerTurn = !gameManager.playerTurn
+    }
+  }, 1000);
+}
+UI.renderBoard(player1.board, 'b-player', gameManager)
+UI.renderBoard(computer.board, 'b-computer', gameManager, true)
--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,6 @@
 const UI = {}
 
-UI.renderBoard = (board, divId, computer = false) => {
+UI.renderBoard = (board, divId, gameManager, computerBoard = false) => {
   const divBoard = document.querySelector(`#${divId}`)
   const size = 500
   divBoard.style.height = `${size}px`
@@ -9,18 +9,35 @@ UI.renderBoard = (board, divId, computer = false) => {
     for (let col = 0; col < 10; col++) {
       const gridCell = document.createElement('div')
       gridCell.classList.add('grid-cell')
-      gridCell.style.height = `${(size/10)-1}px`
-      gridCell.style.width = `${(size/10)-1}px`
-      if (!computer && board.get(row, col)) {
+      if (!computerBoard) {
+        gridCell.id = `${row}-${col}`
+      }
+      gridCell.style.height = `${size / 10}px`
+      gridCell.style.width = `${size / 10}px`
+      if (!computerBoard && board.get(row, col)) {
         gridCell.style.backgroundColor = 'rgb(143, 240, 240)'
       }
       gridCell.addEventListener('click', () => {
+        if (!gameManager.gameOn) return
+        if (
+          !(computerBoard && gameManager.playerTurn) 
+        ) {
+          return
+        }
         if (!board.hasHit(row, col)) {
           let hit = board.receiveAttack(row, col)
           if (hit) {
             gridCell.style.backgroundColor = 'red'
-          } else gridCell.style.backgroundColor = 'rgb(202, 202, 202)'
-          console.log(board.get(row, col))
+            if (board.allSunk()) {
+              gameManager.gameOn = false
+              gameManager.endGame()
+              return
+            }
+          } else  {
+            gridCell.style.backgroundColor = 'rgb(202, 202, 202)'
+            gameManager.playerTurn = !gameManager.playerTurn
+            gameManager.computerPlay()
+          }
         }
       })
       divBoard.appendChild(gridCell)
